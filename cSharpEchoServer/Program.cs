5a190fbd2557c7d48702f0e9c4c0625b0ef4b116
mnentwig <mn83444@gmail.com>
2017-08-27 09:09:02 +0200

    reorganized

---
+++ cSharpEchoServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using octsock5;
+class octsockLoopbackServerDemo {
+    static void Main(string[] args) {
+        octsock5_cl os = new octsock5_cl(portId :"-12345", isServer :true);
+        Console.WriteLine("SERVER_READY"); // agreed token via STDOUT for automated startup
+        os.accept();
+
+        while(true) {
+            dynamic o = os.read();
+            os.write(o);
+            if(o is string)
+                if(o as string == "end loopback and have a nice day")
+                    break;
+        }
+        os.Dispose();
+        Console.WriteLine("SERVER_EXIT");
+    }
+}
